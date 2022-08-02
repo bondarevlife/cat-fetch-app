533fcb5d59ef59e5549c7f42a40f6fab41c02d9b
bondarevlife <bondarevlife@gmail.com>
2022-08-02 07:03:42 +0300

    added favcatpage and allcatpage

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const allCatPageLink = document.querySelector('#allCats')
 const favCatPageLink = document.querySelector('#favCats')
 
 
+allCatPageLink.addEventListener('click', () => {
+    imagesList.textContent = ''
+    allCatPageLink.classList.add("active")
+    favCatPageLink.classList.remove("active")
+    getData();
+    getData();
+    getData();
+})
+
+
+favCatPageLink.addEventListener('click', () => {
+    if (favCatPageLink.classList.contains('active')) return
+
+
+    imagesList.textContent = ''
+    for (let key in localStorage) {
+        if (!localStorage.hasOwnProperty(key)) {
+            continue;
+        }
+        imagesList.insertAdjacentHTML('beforeend', `<li class="image" data-id="${key}">
+        <img src="${localStorage.getItem(key)}" alt="cat" class="img">
+        </li>`)
+    }
+    allCatPageLink.classList.remove("active")
+    favCatPageLink.classList.add("active")
+})
+
 const getData = async () => {
     const resp = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
     const data = await resp.json()
